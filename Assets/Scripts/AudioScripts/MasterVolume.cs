fcd82e03d9a66b599a1831e4287cf7debb4496a3
Jarno <jarwes90@gmail.com>
2020-05-29 11:24:20 +0300

    Volume slider added

---
+++ Assets/Scripts/AudioScripts/MasterVolume.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MasterVolume : MonoBehaviour
+{
+    [Range(0.0f, 1.0f)]
+    [SerializeField]
+
+    public float masterVolume = 1.0f;
+
+
+    void Update()
+    {
+        AudioListener.volume = masterVolume;
+    }
+}
